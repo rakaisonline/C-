7742571b4311cf3120e063581e1128b585b6b0ab
Sudhanshu Ranjan <86006320+sranjan-git@users.noreply.github.com>
2023-12-01 03:58:39 +0530

    Add files via upload

---
+++ if_else/a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    cin>>a;
+    if(a>0){
+        cout<<"A is positive"<<endl;
+    }
+    else{
+        cout<<"A is negative"<<endl;
+    }
+    
+}
---
+++ if_else/c.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin>>a>>b;
+
+    if(a>b){
+        cout<< "A"<<endl;
+    }
+    else{
+        cout << "B"<< endl;
+    }
+    return 0;
+}
